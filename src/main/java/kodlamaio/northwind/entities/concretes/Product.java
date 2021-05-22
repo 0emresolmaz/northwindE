0e697b94b6a1c35b7ca80fc00245fae3ed83a444
saitemresolmaz <76459268+saitemresolmaz@users.noreply.github.com>
2021-05-23 00:00:05 +0300

    Error,Success yönetimi Swagger configurasyonu

--- src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -3,18 +3,23 @@ package kodlamaio.northwind.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
 	@Id
-	@GeneratedValue // id 1'er artırılacak demektir.
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // id 1'er artırılacak demektir.
 	@Column(name = "product_id")
 	private int id;
 
